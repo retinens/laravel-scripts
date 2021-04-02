a858e716dc8812eb19e1b1c06d4c9810834e7a6d
WhereIsLucas <lucas@retinens.be>
2021-04-02 11:30:27 +0200

    fixed a bug

--- add-fontawsesome.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -i
-echo -e "Installing fontawesome-pro"
-npm install --save @fortawesome/fontawesome-pro
-
-echo "@import \"~@fortawesome/fontawesome-pro/css/all.css\";" >> resources/app/sass/vendors/retinens-vendors.scss 
-
-npm run dev
--- delete-repo.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -i
-projectname=rest
-curl -X DELETE  -H "Authorization: token $GH_TOKEN" https://api.github.com/repos/WhereIsLucas/rest
-
-
